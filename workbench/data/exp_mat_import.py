d55fe1e7c55041e74e510747d6b62061ded3070c
FloHofmann <hoflo@web.de>
2025-06-11 09:34:12 +0200

    slight change in gitignore and added preliminary requirements.txt for pip install. Also initialized a module which should be worked on

---
+++ workbench/data/exp_mat_import.py
@@ -0,0 +1,10 @@
+'''
+This module is supposed to enable loading exp_data.mat files,
+originating from the matalb spikesorting function
+'''
+
+import pathlib
+import h5py
+
+if __name__ == "__main__":
+    print('running as main')
